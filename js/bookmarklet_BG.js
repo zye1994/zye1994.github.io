3f0342c607132580b724b82a3e28912b8b05998e
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-11 10:57:44 -0700

    12
    1223

--- js/bookmarklet_BG.js
+++ js/bookmarklet_BG.js
@@ -32,7 +32,7 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-        javascript:void(document.body.style.background = "red";)
+        javascript:void(document.body.style.background = "red" )
 
 
 

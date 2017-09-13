2df20029544d3afca0ef2bf67e4f9afdda05ccbf
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-13 09:57:40 -0700

    1
    1

---
+++ js/bookmarklet_ex.js
@@ -0,0 +1,71 @@
+console.log('Loaded font change bookmarklet!!!');
+
+
+javascript:(function(){
+
+
+var v ="2.2.4"; // version of jquery we want to use
+
+if (window.jQuery== undefined || window.jQuery.fn.jquery < v){
+
+    var done = false;
+    var script = document.createElement("script");
+    script.src="http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js"; // load version of jQuery we specify
+    script.onload = script.onreadystatechange = function(){
+
+        if (!done && (!this.readyState || this.readyState=="loaded" || this.readyState =="complete")){
+
+        done = true;
+        initMyBookmarklet(); //If jquery is loaded now run my script
+
+        }
+    };
+document.getElementsByTagName("head")[0].appendChild(script);
+
+
+}else{
+    initMyBookmarklet();
+}
+
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+$('h1').mousemove(function(){
+
+        var p=document.getElementsByTagName('h1');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+            });
+
+        $(document).click(function(){
+
+        var p=document.getElementsByTagName('p');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=10;p[i].style.fontSize=s+"px"}
+
+
+                var p=document.getElementsByTagName('h1');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+        
+        
+        });
+
+
+    })();
+
+}
+
+})();

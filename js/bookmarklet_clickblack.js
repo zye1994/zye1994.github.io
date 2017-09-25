ef24562c94e70ec3d94ed5299abb105a03e23f7a
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-25 08:11:36 -0700

    11
    111

--- js/bookmarklet_clickblack.js
+++ js/bookmarklet_clickblack.js
@@ -33,7 +33,7 @@ function initMyBookmarklet(){
         // YOUR CODE GOES HERE!
 
             $("p").click(function(){
-                $(this).css("background","black")
+                $(this).css("background","black" "color","black")
              });
 
             $("h1").click(function(){

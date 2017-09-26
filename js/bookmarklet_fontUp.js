f631b75162fe15deef564cfaf84d87fb8b86efb3
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-26 13:31:24 -0700

    qq
    qq

--- js/bookmarklet_fontUp.js
+++ js/bookmarklet_fontUp.js
@@ -36,7 +36,7 @@ var p=document.getElementsByTagName('*');
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
             } else {
-                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+                var s=12;}s-=10;p[i].style.fontSize=s+"px"}
         
 
 

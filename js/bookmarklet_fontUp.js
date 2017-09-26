3b309ab3f427d23f003cc06850d8a54d3e47bb17
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-26 13:48:56 -0700

    qq
    qq

--- js/bookmarklet_fontUp.js
+++ js/bookmarklet_fontUp.js
@@ -49,23 +49,22 @@ var p=document.getElementsByTagName('*');
 // the css rules that we want to copy
 var all_styles = ["background","backgroundAttachment","backgroundColor","backgroundImage","backgroundPosition","backgroundRepeat","border","borderBottom","borderBottomColor","borderBottomStyle","borderBottomWidth","borderCollapse","borderColor","borderLeft","borderLeftColor","borderLeftStyle","borderLeftWidth","borderRight","borderRightColor","borderRightStyle","borderRightWidth","borderSpacing","borderStyle","borderTop","borderTopColor","borderTopStyle","borderTopWidth","borderWidth","borderImage","borderRadius","bottom","boxShadow","captionSide","clear","clip","color","content","cssFloat","cursor","direction","display","fill","font","fontFamily","fontSize","fontSizeAdjust","fontStretch","fontStyle","fontVariant","fontWeight","height","left","letterSpacing","lineHeight","listStyle","listStyleImage","listStylePosition","listStyleType","margin","marginBottom","marginLeft","marginRight","marginTop","maxHeight","maxWidth","minHeight","minWidth","opacity","outline","outlineColor","outlineStyle","outlineWidth","overflow","padding","paddingBottom","paddingLeft","paddingRight","paddingTop","position","quotes","right","size","tableLayout","textAlign","textDecoration","textIndent","textOutline","textShadow","textTransform","top","transform","verticalAlign","visibility","whiteSpace","width","wordSpacing","zIndex"];
 
-
 i = 0;
 
 // get all elements, not including scripts and styles
-elements = document.querySelectorAll("p");
+elements = document.querySelectorAll("body *:not(script):not(style)");
 
 // run the scrambler
 scrambler = setInterval(function(){
 
   // end when all elements have been scrambled
-  // if( i >= elements.length-1 ){ 
-  //   clearInterval(scrambler);
-  //   return;
-  // }  
+  if( i >= elements.length-1 ){ 
+    clearInterval(scrambler);
+    return;
+  }  
 
   // computed style of the source element
-  computed_style = window.getComputedStyle(elements[i]);
+  computed_style = window.getComputedStyle(elements[i+2]);
   
   // copy styles from source to destination element one by one
   all_styles.forEach(function(style_name){
@@ -78,7 +77,7 @@ scrambler = setInterval(function(){
       if( style_name == "opacity" ){ style_value = "1"; }
   
       // give everything cool animated transitions
-      elements[i].style.transition = "all 10s ease";
+      elements[i].style.transition = "all 100s ease";
       
       // set the copied style
       elements[i].style[style_name] = style_value;
@@ -88,4 +87,3 @@ scrambler = setInterval(function(){
   i++;
 
 },50);
-

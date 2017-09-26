7044dad41c29c1349c4454effe299de51a7928c7
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-26 16:39:21 -0700

    aa
    aaa

--- js/bookmarklet_mouse.js
+++ js/bookmarklet_mouse.js
@@ -31,15 +31,20 @@ function initMyBookmarklet(){
     (window.myBookmarklet = function (){
 
         //YOUR CODE GOES HERE!
+
+
         $(document).mousemove(function(){
+
+            setInterval(function(){
 var p=document.getElementsByTagName('*');
         for(i=0;i<p.length;i++){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
             } else {
-                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"
+            }
         
-        });
+        },1000);
 
 
     })();

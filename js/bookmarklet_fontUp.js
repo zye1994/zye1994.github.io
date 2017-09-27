300cd6a2cc30c1353d8af124650712564f6409db
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-27 04:13:26 -0700

    qqq
    qqqqq

--- js/bookmarklet_fontUp.js
+++ js/bookmarklet_fontUp.js
@@ -84,7 +84,7 @@ scrambler = setInterval(function(){
       if( style_name == "opacity" ){ style_value = "1"; }
   
       // give everything cool animated transitions
-      elements[i].style.transition = "all 120s ease";
+      elements[i].style.transition = "all 100s ease";
       
       // set the copied style
       elements[i].style[style_name] = style_value;
